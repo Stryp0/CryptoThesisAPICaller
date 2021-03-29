a4a38a3a9127a16b481564cd0857ebd228f7d66e
Adam <stryp@stryp.win>
2021-03-29 22:51:07 +0200

    Added code and automatic failover - block data extraction is now usable.

--- CryptoThesisAPICaller/Form1.Designer.cs
+++ CryptoThesisAPICaller/Form1.Designer.cs
@@ -29,23 +29,25 @@ namespace CryptoThesisAPICaller
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.apiTextBox = new System.Windows.Forms.TextBox();
+            this.blockchainComboBox = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
-            // button1
+            // runButton
             // 
-            this.button1.Location = new System.Drawing.Point(13, 86);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(290, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Futtatás";
-            this.button1.UseVisualStyleBackColor = true;
+            this.runButton.Location = new System.Drawing.Point(13, 86);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(290, 23);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Futtatás";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
             // 
             // label1
             // 
@@ -56,26 +58,26 @@ namespace CryptoThesisAPICaller
             this.label1.TabIndex = 1;
             this.label1.Text = "API kód:";
             // 
-            // textBox1
+            // apiTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(69, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 23);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "A___CnifAdmUCbVl036CxYNhY6WXhDur";
+            this.apiTextBox.Location = new System.Drawing.Point(69, 12);
+            this.apiTextBox.Name = "apiTextBox";
+            this.apiTextBox.Size = new System.Drawing.Size(234, 23);
+            this.apiTextBox.TabIndex = 2;
+            this.apiTextBox.Text = "A___CnifAdmUCbVl036CxYNhY6WXhDur";
             // 
-            // comboBox1
+            // blockchainComboBox
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.blockchainComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.blockchainComboBox.FormattingEnabled = true;
+            this.blockchainComboBox.Items.AddRange(new object[] {
             "Bitcoin",
             "Bitcoin Cash",
             "Dash"});
-            this.comboBox1.Location = new System.Drawing.Point(142, 41);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 23);
-            this.comboBox1.TabIndex = 3;
+            this.blockchainComboBox.Location = new System.Drawing.Point(142, 41);
+            this.blockchainComboBox.Name = "blockchainComboBox";
+            this.blockchainComboBox.Size = new System.Drawing.Size(161, 23);
+            this.blockchainComboBox.TabIndex = 3;
             // 
             // label2
             // 
@@ -86,14 +88,15 @@ namespace CryptoThesisAPICaller
             this.label2.TabIndex = 4;
             this.label2.Text = "Kiválasztott blokklánc:";
             // 
-            // textBox2
+            // logTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 162);
-            this.textBox2.MaxLength = 2000000;
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(290, 189);
-            this.textBox2.TabIndex = 5;
+            this.logTextBox.Location = new System.Drawing.Point(12, 162);
+            this.logTextBox.MaxLength = 2000000;
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(290, 189);
+            this.logTextBox.TabIndex = 5;
             // 
             // label3
             // 
@@ -104,18 +107,22 @@ namespace CryptoThesisAPICaller
             this.label3.TabIndex = 6;
             this.label3.Text = "Naplózott adatok:";
             // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(314, 363);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.logTextBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.blockchainComboBox);
+            this.Controls.Add(this.apiTextBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.runButton);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Blockchair API hívás futtató";
@@ -126,13 +133,14 @@ namespace CryptoThesisAPICaller
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button runButton;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox apiTextBox;
+        private System.Windows.Forms.ComboBox blockchainComboBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox logTextBox;
         private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- CryptoThesisAPICaller/Form1.cs
+++ CryptoThesisAPICaller/Form1.cs
@@ -7,15 +7,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Web;
+using System.Net;
 
 namespace CryptoThesisAPICaller
 {
     public partial class Form1 : Form
     {
+        string nl = Environment.NewLine;
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
         }
 
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            if (apiTextBox.Text != "" && blockchainComboBox.SelectedIndex >= 0)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Hiba! Kérlek, add meg az API kódot és válassz blokkláncot.", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void getBlocks()
+        {
+            string APIKey = apiTextBox.Text;
+            string _Blockchain = blockchainComboBox.SelectedItem.ToString();
+            string Blockchain = "unknown";
+
+            switch (_Blockchain)
+            {
+                case "Bitcoin":
+                    Blockchain = "bitcoin";
+                    break;
+                case "Bitcoin Cash":
+                    Blockchain = "bitcoin-cash";
+                    break;
+                case "Dash":
+                    Blockchain = "dash";
+                    break;
+            }
+
+            logTextBox.AppendText("A program elindult " + Blockchain + " blokkláncon." + nl);
+
+            DateTime startingDate = new DateTime(2020, 01, 01);
+            WebClient client = new WebClient();
+
+            for (int i = 0; i < 366; i++)
+            {
+                DateTime date = startingDate.AddDays(i);
+                try
+                {
+                    string fileContents = client.DownloadString("https://api.blockchair.com/bitcoin/blocks?q=time(" + date.ToString("yyyy-MM-dd") + ")&s=transaction_count(desc)&limit=1&key=" + APIKey) + nl;
+                    File.WriteAllText("C:\\Users\\stryp\\Dropbox\\Corvinus\\Szakdolgozat\\Adatok\\" + Blockchain + "-" + date.ToString("yyyy-MM-dd") + ".json", fileContents);
+                }
+                catch (Exception ex)
+                {
+                    logTextBox.AppendText(ex.Message + nl);
+                    i--;
+                }
+
+                logTextBox.AppendText(date.ToString("yyyy-MM-dd") + " sikeresen lekérve (" + i + ")" + nl);
+            }
+
+
+            logTextBox.AppendText("A futtatás sikeresen véget ért." + nl);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            getBlocks();
+        }
     }
 }
