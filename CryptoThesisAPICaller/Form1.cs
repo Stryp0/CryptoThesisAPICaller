c3d27e20cae1038d8823e5152a19875a86aa959a
Adam <stryp@stryp.win>
2021-04-19 15:34:43 +0200

    Removed block data gathering and added transaction data gathering to the button handler

--- CryptoThesisAPICaller/Form1.cs
+++ CryptoThesisAPICaller/Form1.cs
@@ -64,7 +64,7 @@ namespace CryptoThesisAPICaller
                 DateTime date = startingDate.AddDays(i);
                 try
                 {
-                    string fileContents = client.DownloadString("https://api.blockchair.com/bitcoin/blocks?q=time(" + date.ToString("yyyy-MM-dd") + ")&s=transaction_count(desc)&limit=1&key=" + APIKey) + nl;
+                    string fileContents = client.DownloadString("https://api.blockchair.com/" + Blockchain + "/blocks?q=time(" + date.ToString("yyyy-MM-dd") + ")&s=transaction_count(desc)&limit=1&key=" + APIKey) + nl;
                     File.WriteAllText("C:\\Users\\stryp\\Dropbox\\Corvinus\\Szakdolgozat\\Adatok\\" + Blockchain + "-" + date.ToString("yyyy-MM-dd") + ".json", fileContents);
                 }
                 catch (Exception ex)
@@ -80,9 +80,60 @@ namespace CryptoThesisAPICaller
             logTextBox.AppendText("A futtatás sikeresen véget ért." + nl);
         }
 
+        private void getTransactions()
+        {
+            string APIKey = apiTextBox.Text;
+            string _Blockchain = blockchainComboBox.SelectedItem.ToString();
+            string Blockchain = "unknown";
+
+            switch (_Blockchain)
+            {
+                case "Bitcoin":
+                    Blockchain = "bitcoin";
+                    break;
+                case "Bitcoin Cash":
+                    Blockchain = "bitcoin-cash";
+                    break;
+                case "Dash":
+                    Blockchain = "dash";
+                    break;
+            }
+
+            logTextBox.AppendText("A program elindult " + Blockchain + " blokkláncon." + nl);
+
+            DateTime startingDate = new DateTime(2020, 01, 01);
+            WebClient client = new WebClient();
+
+            for (int day = 0; day < 366; day++)
+            {
+                DateTime _date = startingDate.AddDays(day);
+                for (int i = 1; i < 25; i += 4)
+                {
+                    DateTime date = _date.AddHours(i);
+                    try
+                    {
+                        // string fileContents = client.DownloadString("https://api.blockchair.com/"+Blockchain+"/blocks?q=time(" + date.ToString("yyyy-MM-dd") + ")&s=transaction_count(desc)&limit=1&key=" + APIKey) + nl;
+                        string fileContents = client.DownloadString("https://api.blockchair.com/" + Blockchain + "/transactions?q=time(.." + date.ToString("yyyy-MM-dd HH:mm:ss") + ")&limit=100&key=" + APIKey) + nl;
+                        File.WriteAllText("C:\\Users\\stryp\\Dropbox\\Corvinus\\Szakdolgozat\\Adatok\\Tranzakciók\\" + Blockchain + "\\" + date.ToString("yyyy-MM-dd") + "-" + i + ".json", fileContents);
+                    }
+                    catch (Exception ex)
+                    {
+                        logTextBox.AppendText(ex.Message + nl);
+                        i--;
+                    }
+
+                    logTextBox.AppendText(date.ToString("yyyy-MM-dd") + "/" + i + " sikeresen lekérve (" + day + "/" + i + ")" + nl);
+                }
+            }
+
+
+            logTextBox.AppendText("A futtatás sikeresen véget ért." + nl);
+        }
+
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            getBlocks();
+            // getBlocks();
+            getTransactions();
         }
     }
 }
